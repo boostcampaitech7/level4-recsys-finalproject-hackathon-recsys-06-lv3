3bd2f5b3aa81b3696ff6d84103b7dc71ba865c27
ChoonB <shgkdc@gmail.com>
2025-01-25 17:12:33 +0900

    feat: MovieLens 20m 전처리 클래스 생성 #68
    - 추상클래스 수정
    - ml20 클래스 생성
    - gitignore에 txt 추가

--- src/preprocess/anime_preprocessor.py
+++ src/preprocess/anime_preprocessor.py
@@ -11,14 +11,16 @@ class AnimePreProcessor(AbstractPreProcessor):
     def __init__(self, dataset: str, data_path, export_path, sample_size: int = 10):
         super().__init__(dataset, data_path, export_path)
         self.data = {
-            "anime_with_synopsis": pd.read_csv(os.path.join(data_path, "anime_with_synopsis.csv")),
+            "anime_with_synopsis": pd.read_csv(
+                os.path.join(data_path, "anime_with_synopsis.csv")
+            ),
             "anime": pd.read_csv(os.path.join(data_path, "anime.csv")),
             "animelist": pd.read_csv(os.path.join(data_path, "animelist.csv")),
             "user_detail": pd.read_csv(os.path.join(data_path, "user_detail.csv")),
         }
         self.sample_size = sample_size
 
-    def pre_process(self) -> None:        
+    def pre_process(self) -> None:
         item_synopsis: pd.DataFrame = self.data["anime_with_synopsis"]
         items: pd.DataFrame = self.data["anime"]
         ratings: pd.DataFrame = self.data["animelist"]
@@ -34,8 +36,13 @@ class AnimePreProcessor(AbstractPreProcessor):
         # ratings 전처리
         ratings.rename(
             columns={
-                "anime_id": "item_id", "user_id": "user_id", "rating": "rating", "watching_status": "watching_status", "watched_episodes": "watched_episodes"
-            }, inplace=True
+                "anime_id": "item_id",
+                "user_id": "user_id",
+                "rating": "rating",
+                "watching_status": "watching_status",
+                "watched_episodes": "watched_episodes",
+            },
+            inplace=True,
         )
         ratings.columns = ratings.columns.str.lower()
 
@@ -53,7 +60,7 @@ class AnimePreProcessor(AbstractPreProcessor):
             "item_synopsis": item_synopsis,
             "train": train,
             "test": test,
-            "users": users
+            "users": users,
         }
 
     def _preprocess_genres(self, items: pd.DataFrame) -> pd.DataFrame:
@@ -63,35 +70,46 @@ class AnimePreProcessor(AbstractPreProcessor):
             lambda row: ",".join(genres_dummies.columns[row == 1]), axis=1
         )
         return items
-    
+
     def _process_item_features(self, items) -> pd.DataFrame:
         replace_features = ["Score", "Episodes", "Ranked"]
         items[replace_features] = items[replace_features].replace("Unknown", np.nan)
         items[replace_features] = items[replace_features].astype(float)
 
-        items["Aired"] = (items["Aired"].apply(
-            lambda x: (re.search(r"\d{4}", x).group(0) if re.search(r"\d{4}", x) else np.nan)
-            ).astype(float)
+        items["Aired"] = (
+            items["Aired"]
+            .apply(
+                lambda x: (
+                    re.search(r"\d{4}", x).group(0)
+                    if re.search(r"\d{4}", x)
+                    else np.nan
+                )
+            )
+            .astype(float)
         )
 
         items["Duration"] = items["Duration"].replace("Unknown", np.nan)
         items["Duration"] = items["Duration"].apply(
             lambda x: int(x.split()[0]) if isinstance(x, str) else x
         )
-        
+
         items.rename(columns={"MAL_ID": "item_id"}, inplace=True)
         items.columns = items.columns.str.lower()
 
         return items
-    
+
     def _preprocess_synopsis(self, item_synopsis) -> pd.DataFrame:
-        item_synopsis["Score"] = item_synopsis["Score"].replace("Unknown", np.nan).astype(float)
-        
-        item_synopsis.rename(columns={"MAL_ID": "item_id", "sypnopsis": "synopsis"}, inplace=True)
+        item_synopsis["Score"] = (
+            item_synopsis["Score"].replace("Unknown", np.nan).astype(float)
+        )
+
+        item_synopsis.rename(
+            columns={"MAL_ID": "item_id", "sypnopsis": "synopsis"}, inplace=True
+        )
         item_synopsis.columns = item_synopsis.columns.str.lower()
 
         return item_synopsis
-    
+
     def _filter_users_with_interactions(self, ratings) -> pd.DataFrame:
         user_rating_counts = ratings["user_id"].value_counts()
         Q1 = user_rating_counts.quantile(0.25)
@@ -102,26 +120,30 @@ class AnimePreProcessor(AbstractPreProcessor):
         filtered_users = user_rating_counts[user_rating_counts <= upper_fence].index
         filtered_ratings = ratings[ratings["user_id"].isin(filtered_users)]
 
-        filtered_ratings.loc[:, 'interaction'] = np.where(
-            ((filtered_ratings['rating'] == 0) | (filtered_ratings['rating'] >= 6)) & 
-            (filtered_ratings['watching_status'] != 4),
-            1, 0
+        filtered_ratings.loc[:, "interaction"] = np.where(
+            ((filtered_ratings["rating"] == 0) | (filtered_ratings["rating"] >= 6))
+            & (filtered_ratings["watching_status"] != 4),
+            1,
+            0,
         )
 
-        user_interaction_counts = (
-            filtered_ratings[filtered_ratings['interaction'] == 1]['user_id'].value_counts()
-        )
-        filtered_users_with_interactions = user_interaction_counts[user_interaction_counts >= 11].index
+        user_interaction_counts = filtered_ratings[
+            filtered_ratings["interaction"] == 1
+        ]["user_id"].value_counts()
+        filtered_users_with_interactions = user_interaction_counts[
+            user_interaction_counts >= 11
+        ].index
 
         return filtered_ratings[
             filtered_ratings["user_id"].isin(filtered_users_with_interactions)
         ]
-    
+
     def _split_train_test(self, ratings) -> tuple:
         test = (
             ratings[ratings["interaction"] == 1]
             .groupby("user_id")
-            .apply(lambda x: x.sample(n=self.sample_size, random_state=42)).reset_index(drop=True)
+            .apply(lambda x: x.sample(n=self.sample_size, random_state=42))
+            .reset_index(drop=True)
         )
 
         train = ratings[
