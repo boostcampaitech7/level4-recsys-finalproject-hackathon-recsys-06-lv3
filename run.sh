ad4cb1057983b558f84ccc1173e6b4aa9c70e3d0
Seongyun <seongyungim99@gmail.com>
2025-01-31 15:37:31 +0900

    feat: run.sh 추가

---
+++ run.sh
@@ -0,0 +1,8 @@
+# MovieLens-20m
+
+# SASRec+
+# python app.py dataset.max_length=200 SASRec.hidden_units=256
+# SASRec+ 3000
+# python app.py dataset.max_length=200 +dataset.num_negatives=3000 SASRec.hidden_units=256
+# SASRec vanilla
+# python app.py dataset.max_length=200 +seqrec_module.loss=bce +dataset.num_negatives=1 dataset.full_negative_sampling=True SASRec.hidden_units=256
